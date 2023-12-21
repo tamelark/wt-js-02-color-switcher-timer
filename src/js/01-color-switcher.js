7bd3047c4d32690b1bb3539fad7efbef87e0895e
Jemin Kipkin <tamelarkdagora@gmail.com>
2023-12-21 23:05:05 +0200

    What is it good for?

---
+++ src/js/01-color-switcher.js
@@ -0,0 +1,27 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
+
+const dataStart = document.querySelector('button[data-start]');
+const dataStop = document.querySelector('button[data-stop]');
+const bodyEl = document.querySelector('body');
+
+let timerId = null;
+dataStop.setAttribute('disabled', true);
+
+// додати код для генерування випадкового кольору
+
+dataStart.addEventListener('click', () => {
+  dataStop.removeAttribute('disabled');
+  timerId = setInterval(() => {
+    const color = getRandomHexColor();
+    bodyEl.style.backgroundColor = color;
+  }, 1000);
+  dataStart.setAttribute('disabled', true);
+});
+
+dataStop.addEventListener('click', () => {
+  dataStart.removeAttribute('disabled');
+  clearInterval(timerId);
+  dataStop.setAttribute('disabled', true);
+});
